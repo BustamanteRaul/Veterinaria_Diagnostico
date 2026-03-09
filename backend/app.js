a522fd420e06c7010060419796ba1e09b734aeb2
Raul Bustamante Alba <bustamanteraul039@gmail.com>
2026-03-09 17:32:46 -0300

    creando server

---
+++ backend/app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const session = require("express-session");
+const cors = require("cors");
+
+// Conectar con el frontend
+app.use(
+  cors({
+    origin: ["http://localhost:5173"], //puerto en donde react se aloja
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+// Configurar la sesión
+app.use(
+  session({
+    secret: "secretkey123",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      secure: false, 
+      maxAge: 1000 * 60 * 60 * 24, 
+    },
+  })
+);
+
+// abrir el servidor
+app.listen(3000, () => {
+  console.log("Servidor corriendo en http://localhost:3000");
+});
+
+module.exports = app;
